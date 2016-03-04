0c73fdae0cfb9612b9fb9422aadab68800cd0035
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-03-04 14:44:26 +0900

    generate bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Acme\OAuth2ClientBundle\AcmeOAuth2ClientBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Acme/OAuth2ClientBundle/AcmeOAuth2ClientBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\OAuth2ClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeOAuth2ClientBundle extends Bundle
+{
+}
